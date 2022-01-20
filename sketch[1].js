aab2d69cd21114772eb720384b32c9e390c2f148
Liam <90276819+liam-fletcher1@users.noreply.github.com>
2022-01-20 13:42:02 -0500

    Update sketch[1].js

--- sketch[1].js
+++ sketch[1].js
@@ -34,3 +34,40 @@ function setup() {
   // frameRate(1000)
   angleMode(DEGREES)
 }
+
+function draw() {
+  background(colorBagground)
+
+  //   Red lines
+  strokeWeight(10)
+  stroke(colorComputer)
+  line(7, 16, 7, height / 2 - 7)
+  line(width - 7, 16, width - 7, height / 2 - 7)
+  line(16, 7, width / 2 - 75, 7)
+  line(width - 16, 7, width / 2 + 75, 7)
+
+  //   blue lines
+  stroke(yourColor)
+  line(7, height / 2 + 7, 7, height - 16)
+  line(width - 7, height / 2 + 7, width - 7, height - 16)
+  line(16, height - 7, width / 2 - 75, height - 7)
+  line(width - 16, height - 7, width / 2 + 75, height - 7)
+
+  //   white lines
+  noFill()
+  stroke(colorLines)
+  strokeWeight(5)
+  ellipse(width / 2, height / 2, 150)
+  ellipse(width / 2, height / 2, 5)
+  line(16, height / 2, width - 16, height / 2)
+  arc(width / 2, 16, 150, 150, 0, 180, OPEN)
+  arc(width / 2, height - 16, 150, 150, 180, 0, OPEN)
+
+  //   pallet player
+  if (mouseX < width - palletW - 7 && mouseX > palletW + 7 && mouseY > height / 2 + palletW && mouseY < height - palletW - 7) {
+    paletX = mouseX
+    paletY = mouseY
+  }
+  noStroke()
+  fill(yourColor)
+  ellipse(paletX, paletY, palletW * 2)
